e6edf5e10417efa877e1df38b024d44f304c8822
alpacino609 <https://github.com/settings/profile>
2025-09-16 13:57:24 +0900

    hi3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -17,35 +17,36 @@ const Login = ({
     // loginApi(userUid, password);
     console.log("userUid", userUid);
     console.log("password", password);
-    try {
-      const response = await axios.post(
-        "http://localhost:5000/users/login",
-        { userUid, password },
-        { withCredentials: true } // 세션 쿠키 브라우저에 저장
-      );
-      // 🔹 수정 부분: 백엔드에서 전달하는 메시지와 status 활용
-      if (response.status === 200) {
-        // 백엔드가 보낸 message 그대로 사용
-        setBeingLogined(true);
-        setPassword("");
-        setModalTitle(response.data.message); // 🔹 수정: "로그인 성공" 대신 백엔드 메시지
-        setModalContent("");
-        openModal();
-        console.log("response", response);
-      }
-    } catch (error) {
-      console.log("error", error);
-      setBeingLogined(false);
+    setBeingLogined(true);
+    // try {
+    //   const response = await axios.post(
+    //     "http://localhost:5000/users/login",
+    //     { userUid, password },
+    //     { withCredentials: true } // 세션 쿠키 브라우저에 저장
+    //   );
+    //   // 🔹 수정 부분: 백엔드에서 전달하는 메시지와 status 활용
+    //   if (response.status === 200) {
+    //     // 백엔드가 보낸 message 그대로 사용
+    //     setBeingLogined(true);
+    //     setPassword("");
+    //     setModalTitle(response.data.message); // 🔹 수정: "로그인 성공" 대신 백엔드 메시지
+    //     setModalContent("");
+    //     openModal();
+    //     console.log("response", response);
+    //   }
+    // } catch (error) {
+    //   console.log("error", error);
+    //   setBeingLogined(false);
 
-      // 🔹 수정: error.message 또는 response.data.error만 표시
-      if (error.response) {
-        setModalTitle(error.response.data.error || "로그인 실패");
-      } else {
-        setModalTitle(error.message);
-      }
-      setModalContent("");
-      openModal();
-    }
+    //   // 🔹 수정: error.message 또는 response.data.error만 표시
+    //   if (error.response) {
+    //     setModalTitle(error.response.data.error || "로그인 실패");
+    //   } else {
+    //     setModalTitle(error.message);
+    //   }
+    //   setModalContent("");
+    //   openModal();
+    // }
   };
   const onChangeForUserUid = (event) => {
     setUserUid(event.target.value);
@@ -90,7 +91,9 @@ const Login = ({
   } else {
     return (
       <>
-        <p>로그인 성공상태</p>
+        <div className="login-complete">
+          <p>로그인 성공상태</p>
+        </div>
       </>
     );
   }
